efc5f080762dacd7b0d5bdcebd755df9fabb334f
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-06-22 19:07:33 -0500

    Add auth route

---
+++ api/src/routes/auth.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs"); 
+
+let users = [
+    {
+        email: "jpvazquez@espol.edu.ec",
+        password: "$2y$07$IQGI0XRhgczqKz9sZKkHc.Jzq5vbv1Bc2HLbm1r5.8H8SpuNGiuea"
+    },
+    {
+        email: "josdavil@espol.edu.ec",
+        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq"
+    },
+    {
+        email: "jmluna@espol.edu.ec",
+        password: "$2y$07$P4FURPfVXKJMLSORV0rRFuVm9qrVb1/njfHyNYwspWtfdaQ016rFi"
+    },
+    {
+        email: "rodemore@espol.edu.ec",
+        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq"
+    },
+    {
+        email: "alriofri@espol.edu.ec ",
+        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq"
+    },
+
+]
+
+let getUser = (email)=>{
+    let users_found = users.find(function(element, index) {
+        if(element.email == email)
+            return true;
+    });
+    if (users_found){
+        return users_found
+    }
+    throw new Error({'message':'Invalid email or password'});
+}
+
+router.post('/', async (req, res, next) => {
+    try {    
+        let user = getUser(req.body.email)
+        if(bcrypt.compareSync(req.body.password,user.password)){
+            let token = jwt.sign( {email: user.email}, "SECRETTOKEN", {expiresIn: '24h'});
+            return res.json({auth:true, token:token});
+            
+        }
+        throw new Error({'message':'Invalid email or password'});
+    } catch (err) {
+        return res.status(404).json({"auth": false, "message": "Contraseña o correo incorrectos"});
+    }
+});
+
+
+module.exports = router;
+
+
+
+
+
