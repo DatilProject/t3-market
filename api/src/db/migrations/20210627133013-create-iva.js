9598f7d1873340645da7657d5dfa2b082d3baa8f
jpvazque <G1thub2020>
2021-06-27 18:38:57 -0500

    Update migrations files

--- api/src/db/migrations/20210627133013-create-iva.js
+++ api/src/db/migrations/20210627133013-create-iva.js
@@ -4,7 +4,7 @@ module.exports = {
     await queryInterface.createTable('iva', {
       id: {
         allowNull: false,
-        autoIncrement: true,
+        autoIncrement: false,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
