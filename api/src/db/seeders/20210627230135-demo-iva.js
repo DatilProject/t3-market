1a43cbee529cd80e8724a88975e2c6b298b3bd83
jpvazque <G1thub2020>
2021-06-27 18:39:42 -0500

    Add seeders for testing database tables

--- api/src/db/seeders/20210627230135-demo-iva.js
+++ api/src/db/seeders/20210627230135-demo-iva.js
@@ -3,33 +3,19 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      'product',
+      'iva',
       [
         {
           id: 1,
-          main_code:'12345',
-          description: '',
-          name: 'XBOX',
-          on_sale: false,
-          price: 205.50,
-          stock: 34,
-          ice_id: 1,
-          iva_id: 1,
-          market_id:1,
+          name:'12%',
+          percentage: 0.12,
           created_at:new Date(),
           updated_at: new Date(),
         },
         {
           id: 2,
-          main_code:'67890',
-          description: '',
-          name: 'Play5',
-          on_sale: true,
-          price: 400.00,
-          stock: 97,
-          ice_id: 1,
-          iva_id: 1,
-          market_id:1,
+          name:'0%',
+          percentage: 0.00,
           created_at:new Date(),
           updated_at: new Date(),
         }
--- api/src/db/seeders/20210627232017-demo-market.js
+++ api/src/db/seeders/20210627232017-demo-market.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      'commerce',
+      'market',
       [
         {
           business_name:'electronix',
@@ -12,16 +12,6 @@ module.exports = {
           phone: '0987656789',
           trade_name: 'electronix',
           market_id: 1,
-          created_at:new Date(),
-          updated_at: new Date(),
-        },
-        {
-          business_name:'Don Pepe',
-          email: 'don_pepe@hotmail.com',
-          id_type: 'RUC',
-          phone: '0982346632',
-          trade_name: 'Don Pepe',
-          market_id: 2,
           created_at:new Date(),
           updated_at: new Date(),
         }
