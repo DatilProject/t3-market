11cdef53a6ccc5bffa96394187d4dfbad04ded7d
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-06-28 02:06:05 -0500

    Update product routes

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -26,4 +26,31 @@ router.get('/', async (req, res, next) => {
     res.status(200).json(products)
 });
 
+router.get('/commerce/:id', async (req, res, next) => {
+
+    const products =  await Product.findAll({
+        where: {
+            marketId: req.params.id
+        },
+        include:[{
+            model: Category,
+            attributes: ['name']
+        }, 
+        {
+            model: Iva,
+            attributes: ['name', 'percentage']
+        }, 
+        {
+            model: Ice,
+            attributes: ['name', 'percentage']
+        }, 
+        {
+            model: Image,
+            attributes: ['location']
+        }, 
+        ]
+        });
+    res.status(200).json(products)
+});
+
 module.exports = router;
