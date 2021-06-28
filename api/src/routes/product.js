abd8ce9f8f1bed5a081bf281eb1f3bb4573b87a0
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-06-28 02:27:45 -0500

    Add POST and PUT routes for product

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -53,4 +53,30 @@ router.get('/commerce/:id', async (req, res, next) => {
     res.status(200).json(products)
 });
 
+router.post('/', async (req, res, next) => {
+    try{
+        const newProduct = await Product.create(req.body);
+        res.status(200).send(newProduct);
+    }
+    catch{
+        res.status(404).send({updated: false, message:'Producto no pudo ser agregado'})
+    }
+
+});
+
+router.put('/:id', async (req,res,next)=>{
+    const product = await Product.update(req.body , {
+        where: {
+          id: req.params.id
+        }
+      });
+      
+    if(!product){
+        res.status(404).send({updated: false, message:'Producto no pudo ser actualizado.'})
+    }else{
+        
+        res.status(200).json({updated: true, message: "Se ha actualizado correctamente"});
+    }
+    });
+
 module.exports = router;
