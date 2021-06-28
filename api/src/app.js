38f5cd4080a0cf278481d2b4d2292cf1b22079e1
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-06-28 02:01:20 -0500

    Add cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const db = require('./db/db');
+const cors = require('cors')
 require('dotenv').config();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+app.options('*', cors());
 
 app.set('port' , process.env.PORT || 3000);
 
