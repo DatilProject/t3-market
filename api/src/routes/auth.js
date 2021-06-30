3c4d6eb718703543212349b1466ba3cd7a311506
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-06-30 17:39:48 -0500

    Update auth route

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -5,24 +5,40 @@ const bcrypt = require("bcryptjs");
 
 let users = [
     {
-        email: "jpvazquez@espol.edu.ec",
-        password: "$2y$07$IQGI0XRhgczqKz9sZKkHc.Jzq5vbv1Bc2HLbm1r5.8H8SpuNGiuea"
-    },
-    {
-        email: "josdavil@espol.edu.ec",
-        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq"
+        email: "jedavalo@espol.edu.ec",
+        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq",
+        id: 2,
+        username: 'jedavalo',
+        name: "Josue Dávalos",
+        phone: "0995091644",
+        address: "Mapasingue"
     },
     {
         email: "jmluna@espol.edu.ec",
-        password: "$2y$07$P4FURPfVXKJMLSORV0rRFuVm9qrVb1/njfHyNYwspWtfdaQ016rFi"
+        password: "$2y$07$P4FURPfVXKJMLSORV0rRFuVm9qrVb1/njfHyNYwspWtfdaQ016rFi",
+        id: 3,
+        username: 'jmluna',
+        name: "Jocellyn Luna",
+        phone: "0995091644",
+        address: "Guasmo sur"
     },
     {
         email: "rodemore@espol.edu.ec",
-        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq"
+        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq",
+        id: 1,
+        username: 'rodemore',
+        name: "Robert Moreno",
+        phone: "0995091644",
+        address: "Prosperina Coop. 31 Agosto"
     },
     {
         email: "alriofri@espol.edu.ec ",
-        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq"
+        password: "$2y$07$Nt2VpZTwfvTpeLaLE78hGeueiOBQfBU4663yNudro5mxcPEMM7.Wq",
+        id: 4,
+        username: 'alriofri',
+        name: "Adriana Riofrío",
+        phone: "0995091644",
+        address: "Prosperina Coop. 31 Agosto"
     },
 
 ]
