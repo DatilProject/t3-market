73c823fd97e8e2b0efec8caafb26e2b2979d6fef
Jocellyn Luna <josiemoon@msn.com>
2021-07-30 02:42:49 -0500

    extra assets

---
+++ Movil/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Color(0xFFE4E8EA);
+const bgColor = Color(0xFFFFFFFF);
+
+const defaultPadding = 16.0;
