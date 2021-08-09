14af7cc6852536f42ba096e38b8217f640ef6a75
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-08-09 16:06:39 -0500

    Update product route

--- api/src/middlewares/products.js
+++ api/src/middlewares/products.js
@@ -1,16 +1,24 @@
 
 const moment= require('moment');
 
-getDiscountPrice = (product) =>{
-    return product["price"] - product["price"]* product["discount"]/100 
+getDiscountedPrice = (product) =>{
+    return roundToTwo(product["price"] - product["price"]*product["discount"]/100 )
 }
 
+roundToTwo = (num) => {    
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
+getDiscount = (product) =>{
+    return roundToTwo(product["discount"]*product["price"]/100)
+}
 
 preProcessProducts = (productList) =>{
     let today = moment();
     return productList
         .map(p => ({...p, originPrice: p.price, 
-                          discountPrice: getDiscountPrice(p),
+                          discountPrice: getDiscount(p),
+                          price: getDiscountedPrice(p), 
                           isNew: p.updatedAt >= today.subtract(7, 'days')
                         }));
 
