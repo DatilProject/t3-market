74af2068cee39e54ea6678bf63f6942d7e939732
Josue Davalos <jedavalo@espol.edu.ec>
2021-08-09 17:52:12 -0500

    Add Test for marlet and catalogue

---
+++ Web/src/__test__/components/Market.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ProviderMock from "../___mocks__/ProviderMock";
+import Market from "../../components/market/Market";
+
+describe("Test Login Page", () => {
+	test("Render Login Page", () => {
+		const login = shallow(
+			<ProviderMock>
+				<Market />
+			</ProviderMock>,
+		);
+		expect(login.length).toEqual(1);
+	});
+});
