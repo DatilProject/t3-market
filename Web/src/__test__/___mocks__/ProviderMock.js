3a5f645f252a336ab336deff56db99f03556e4c7
Josue Davalos <jedavalo@espol.edu.ec>
2021-08-09 17:44:21 -0500

    Improve test

---
+++ Web/src/__test__/___mocks__/ProviderMock.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Router } from "react-router";
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import generateStore from "../../redux/store";
+
+const store = generateStore();
+const history = createBrowserHistory();
+
+const ProviderMock = (props) => {
+	<Provider store={store}>
+		<Router history={history}>{props.children}</Router>
+	</Provider>;
+};
+
+export default ProviderMock;
--- Web/src/__test__/components/SideBar.test.js
+++ Web/src/__test__/components/SideBar.test.js
@@ -1,27 +1,32 @@
 import React from "react";
+import { mount } from "enzyme";
 import { render, screen } from "@testing-library/react";
-import SideBar from "../components/common/sidebar/SideBar";
+import SideBar from "../../components/common/sidebar/SideBar";
 
-describe("SideBar Page Sections", () => {
-	it("Must display a Panel section", () => {
+describe("Test SideBar Component", () => {
+	test("Render of the SideBar component", () => {
+		const sidebar = mount(<SideBar />);
+		expect(sidebar.length).toEqual(1);
+	});
+	test("Must display a Panel section", () => {
 		render(<SideBar />);
 		const linkElement = screen.getByText(/Panel/i);
 		expect(linkElement).toBeInTheDocument;
 	});
 
-	it("Must display a Catalogue section", () => {
+	test("Must display a Catalogue section", () => {
 		render(<SideBar />);
 		const linkElement = screen.getByText(/Catalogo/i);
 		expect(linkElement).toBeInTheDocument;
 	});
 
-	it("Must display a Market section", () => {
+	test("Must display a Market section", () => {
 		render(<SideBar />);
 		const linkElement = screen.getByText(/Market/i);
 		expect(linkElement).toBeInTheDocument;
 	});
 
-	it("Must display a logout section", () => {
+	test("Must display a logout section", () => {
 		render(<SideBar />);
 		const linkElement = screen.getByText(/Cerrar Sesión/i);
 		expect(linkElement).toBeInTheDocument;
