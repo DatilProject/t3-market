492898d23abf1970a395c87e4c68623d46c1c60b
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-08-10 03:27:11 -0500

    Add route for sales report by category

--- api/src/routes/reports.js
+++ api/src/routes/reports.js
@@ -2,24 +2,32 @@ var express = require('express');
 var router = express.Router();
 const {conn} = require("../db/db");
 
-router.get('/sales/:range', async (req, res, next) => {
+router.get('/sales/commerce/:id/:range', async (req, res, next) => {
     switch (req.params.range) {
         case "1":
             query= `select date(b."date"), SUM(b.subtotal) as sales 
             from bills b
+            inner join orders o on b."orderId" = o.id
+            where o."marketId" = ${req.params.id}
             group by date(b."date")
             ORDER BY date DESC;`
+
             break
         case "2":
             query = `select EXTRACT(YEAR FROM b.date) as year, 
             EXTRACT(MONTH FROM b.date) as "month", SUM(b.subtotal) as sales 
             from bills b
+            inner join orders o on b."orderId" = o.id
+            where o."marketId" = ${req.params.id}
             group by year, month
             order by year desc,month desc;`
             break
         case "3":
             query = `select EXTRACT(YEAR FROM b.date) as year, 
-            SUM(b.subtotal) as sales from bills b
+            SUM(b.subtotal) as sales 
+            from bills b
+            inner join orders o on b."orderId" = o.id
+            where o."marketId" = ${req.params.id}
             group by year
             order by year desc;`
             break
@@ -32,11 +40,27 @@ router.get('/sales/:range', async (req, res, next) => {
     res.status(200).json(results)
 });
 
-router.get('/profit/:range', async (req, res, next) => {
+router.get('/salesbycategory/commerce/:id', async (req, res, next) => {
+    let query = `select c.name, Sum(p.price - p.price * p.discount /100) as sales
+    from orders o
+    inner join items i on i."orderId" = o.id
+    inner join products p on p.id = i."productId"
+    inner join categories c on p."categoryId"  = c.id
+    where o.is_paid_up = true and o."marketId" = ${req.params.id}
+    group by c."name" `
+    
+    const [results, metadata] = await conn.query(query);
+    res.status(200).json(results)
+});
+
+
+router.get('/profit/commerce/:id/:range', async (req, res, next) => {
     switch (req.params.range) {
         case "1":
             query= `select date(b."date"), SUM(b.subtotal)*0.30 as profit 
             from bills b
+            inner join orders o on b."orderId" = o.id
+            where o."marketId" = ${req.params.id}
             group by date(b."date")
             ORDER BY date DESC;`
             break
@@ -44,12 +68,17 @@ router.get('/profit/:range', async (req, res, next) => {
             query = `select EXTRACT(YEAR FROM b.date) as year, 
             EXTRACT(MONTH FROM b.date) as "month", SUM(b.subtotal) * 0.3 as profit 
             from bills b
+            inner join orders o on b."orderId" = o.id
+            where o."marketId" = ${req.params.id}
             group by year, month
             order by year desc,month desc;`
             break
         case "3":
             query = `select EXTRACT(YEAR FROM b.date) as year, 
-            SUM(b.subtotal)*0.3 as profit from bills b
+            SUM(b.subtotal)*0.3 as profit
+            from bills b
+            inner join orders o on b."orderId" = o.id 
+            where o."marketId" = ${req.params.id}
             group by year
             order by year desc;`
             break
@@ -61,6 +90,8 @@ router.get('/profit/:range', async (req, res, next) => {
     const [results, metadata] = await conn.query(query);
     res.status(200).json(results)
 });
+
+
 
 
 module.exports = router;
