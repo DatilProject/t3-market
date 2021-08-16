9b5c52b1c9461b76d74ba8f887f9efd94c4ca812
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-08-16 00:52:25 -0500

    Update order router

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -97,7 +97,7 @@ router.post('/commerce/:commerceId/:clientId', async (req, res, next) => {
     res.status(200).json(newOrder)
 });
 
-router.post("/item/:itemId'", async (req, res) => {    
+router.post("/updateitem/:itemId'", async (req, res) => {    
     const item = await Item.create({ id: req.params.itemId });
     item.quantity = req.body.quantity
     await item.save();
