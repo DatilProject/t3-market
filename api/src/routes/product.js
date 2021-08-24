98a911f91789ca5aa089c7ca49912786bf8e824d
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2021-08-24 00:34:17 -0500

    Update product router

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -60,6 +60,7 @@ router.get('/commerce/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try{
+        console.log(req.body);
         const newProduct = await Product.create(req.body).then(async (p)=>{
             var base64Data = req.body.images.replace(/^data:image\/png;base64,/, "");
             fs.writeFile(`src/public/products/${p.id}.png`, base64Data, 'base64', function(err) {
